6a15ae87c5c824938e21af645253d7c6b400e297
Scott Turnbull <streamweaver@gmail.com>
2025-08-25 18:11:13 -0400

    feat: add viewport meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
